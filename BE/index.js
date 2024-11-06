bfcf2c4ef6b4fbc2019927f2c3eee1a5e9800b28
hiepbao <cubi27395@gmail.com>
2024-11-06 13:49:00 +0700

    v0.2 BE

--- BE/index.js
+++ BE/index.js
@@ -10,7 +10,6 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Kết nối MySQL
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
@@ -27,26 +26,33 @@ db.connect((err) => {
   console.log('Connected to MySQL database.');
 });
 
-// Route cho trang HOME
 app.get('/', (req, res) => {
   res.render('home');
 });
 
-// Route cho trang "Get in Touch"
 app.get('/contact', (req, res) => {
   res.render('contact');
 });
 
-// Xử lý dữ liệu từ form "Get in Touch"
+app.get('/home', (req, res) => {
+  const query = 'SELECT * FROM `get_in_touch_Tong_Bao_Hiep` WHERE 1';
+
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('Error fetching data:', err);
+      return res.status(500).send('An error occurred while fetching the data.');
+    }
+    res.render('home', { data: results });
+  });
+});
+
 app.post('/contact', (req, res) => {
   const { name, email, country, company_name, message } = req.body;
 
-  // Kiểm tra các trường bắt buộc
   if (!name || !email || !company_name || !message ) {
     return res.status(400).send('All required fields must be filled!');
   }
 
-  // Chèn dữ liệu vào bảng get_in_touch_your_name
   const query = 'INSERT INTO get_in_touch_Tong_Bao_Hiep (name, email, country, company_name, message) VALUES (?, ?, ?, ?, ?)';
   db.query(query, [name, email, country, company_name, message], (err, result) => {
     if (err) {
@@ -57,7 +63,6 @@ app.post('/contact', (req, res) => {
   });
 });
 
-// Khởi chạy server
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
